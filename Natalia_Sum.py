eb56bc579f2f86c075630fb27ac9979eefe7d228
Nataliie23 <naroriran@gmail.com>
2025-09-24 16:01:40 +0800

    Create snipetts

--- Natalia_Sum.py
+++ Natalia_Sum.py
@@ -8,4 +8,5 @@
 
 # 2. Process
 
-# 3. Output
+# 3. Output 
+
---
+++ Natalia_minus.py
@@ -0,0 +1,11 @@
+#
+# Natalia, 2025/09/24
+# File: Natalia_minus.py
+# Short description of the task
+#
+
+# 1. Input
+
+# 2. Process
+
+# 3. Output
